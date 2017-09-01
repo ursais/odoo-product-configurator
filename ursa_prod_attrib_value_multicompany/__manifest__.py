89e6d1f1b55a9d72da2117c1b4ab997ff6afbb93
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-09-01 12:33:45 +0530

    [ADD]13342: ursa_prod_attrib_value_multicompany module has been added

---
+++ ursa_prod_attrib_value_multicompany/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 - TODAY, Ursa Information Systems
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Ursa Product Attribute Value Multi-Company",
+    "summary": "To support multi-company rule on attribute value",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ursa Information Systems",
+    "category": "Sales",
+    "maintainer": "Ursa Information Systems",
+    "website": "http://www.ursainfosystems.com",
+    "depends": [
+        "product_configurator"
+    ],
+    "qweb": [
+    ],
+    "data": [
+        "views/product_attribute_views.xml",
+        "security/attribute_value_security.xml",
+    ],
+    "application": False,
+}
---
+++ ursa_prod_attrib_value_multicompany/models/product_attribute.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 - TODAY, Ursa Information Systems
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class ProductAttributeValue(models.Model):
+    _inherit = "product.attribute.value"
+
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id)

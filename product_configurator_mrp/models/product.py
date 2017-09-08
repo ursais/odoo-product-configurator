ae055311b9cab7eb8fd165447db4a40224e2f2a9
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-09-08 22:50:03 +0530

    [IMP]13341: update product variant price based on new BoM lines qty

--- product_configurator_mrp/models/product.py
+++ product_configurator_mrp/models/product.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, api
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class ProductTemplate(models.Model):
@@ -15,16 +17,23 @@ class ProductTemplate(models.Model):
         variant = super(ProductTemplate, self).create_get_variant(
             value_ids, custom_values=custom_values
         )
-        line_vals = [
-            (0, 0, {'product_id': attribute_value.product_id.id,
-                    'product_qty': self._context.get('wizard_values') and self._context.get('wizard_values').get(attribute_value.id,1) or 1
-                    }) for attribute_value in variant.attribute_value_ids
-        ]
+        #Prepare BoM lines and calculate product variant price
+        line_vals = []
+        price = 0
+        for attribute_value in variant.attribute_value_ids:
+            product_qty = self._context.get('wizard_values') and self._context.get('wizard_values').get(attribute_value.id,1) or 1
+            line_vals.append((0, 0, {'product_id': attribute_value.product_id.id,
+                                     'product_qty': product_qty,
+                                     }))
+            price += attribute_value.product_id.lst_price * (product_qty-1)
         values = {
             'product_tmpl_id': self.id,
             'product_id': variant.id,
             'bom_line_ids': line_vals
         }
+        # Set Variant price with new calculated Price and extra price as 0
+        variant.list_price = price
+        _logger.info("Variant price %s."%variant.lst_price)
         self.env['mrp.bom'].create(values)
 
         return variant

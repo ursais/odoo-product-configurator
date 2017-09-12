cf09c663d0219802403611e819849806e4981c1c
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-09-12 15:07:12 +0530

    [pycharm]code formatting product_configurator_mrp

--- product_configurator_mrp/models/product.py
+++ product_configurator_mrp/models/product.py
@@ -2,6 +2,7 @@
 
 from odoo import models, api
 import logging
+
 _logger = logging.getLogger(__name__)
 
 
@@ -9,7 +10,8 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.multi
-    def create_get_variant(self, value_ids, custom_values=None, website_values={}):
+    def create_get_variant(self, value_ids, custom_values=None,
+                           website_values={}):
         """Add bill of matrials to the configured variant."""
         if custom_values is None:
             custom_values = {}
@@ -17,22 +19,24 @@ class ProductTemplate(models.Model):
         variant = super(ProductTemplate, self).create_get_variant(
             value_ids, custom_values=custom_values
         )
-        #Prepare BoM lines and calculate product variant price
+        # Prepare BoM lines and calculate product variant price
         wizard_values = {}
         line_vals = []
         price = 0
-        
+
         if website_values:
             wizard_values = website_values
         else:
             wizard_values = self._context.get('wizard_values', {})
-        
+
         for attribute_value in variant.attribute_value_ids:
-            product_qty = wizard_values and int(wizard_values.get(attribute_value.id,1)) or 1
-            line_vals.append((0, 0, {'product_id': attribute_value.product_id.id,
-                                     'product_qty': product_qty,
-                                     }))
-            price += attribute_value.product_id.lst_price * (product_qty-1)
+            product_qty = wizard_values and int(
+                wizard_values.get(attribute_value.id, 1)) or 1
+            line_vals.append(
+                (0, 0, {'product_id': attribute_value.product_id.id,
+                        'product_qty': product_qty,
+                        }))
+            price += attribute_value.product_id.lst_price * (product_qty - 1)
         values = {
             'product_tmpl_id': self.id,
             'product_id': variant.id,
@@ -40,6 +44,6 @@ class ProductTemplate(models.Model):
         }
         # Set Variant price with new calculated Price and extra price as 0
         variant.list_price = price
-        _logger.info("Variant price %s."%variant.lst_price)
+        _logger.info("Variant price %s." % variant.lst_price)
         self.env['mrp.bom'].create(values)
         return variant

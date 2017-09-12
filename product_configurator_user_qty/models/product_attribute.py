075a29869535658b05eb8cc2110f97732890a180
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-09-12 15:00:21 +0530

    [pycharm]code formatting product_configurator_user_qty

--- product_configurator_user_qty/models/product_attribute.py
+++ product_configurator_user_qty/models/product_attribute.py
@@ -8,13 +8,14 @@ class ProductAttributeLine(models.Model):
     _inherit = 'product.attribute.line'
 
     user_qty = fields.Boolean(compute='_is_user_qty',
-                                  string="User Qty", store=True)
+                              string="User Qty", store=True)
 
     @api.depends('value_idss.is_user_qty')
     def _is_user_qty(self):
         for line in self:
             user_qty_lines = [user_qty for user_qty in
-                             line.value_idss.mapped('is_user_qty') if user_qty]
+                              line.value_idss.mapped('is_user_qty') if
+                              user_qty]
             if user_qty_lines:
                 line.user_qty = True
             else:
